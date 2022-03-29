a2518a58b0c3f1843a3486803ea4cc3a7308679b
Wendy Chan <wenwkich@gmail.com>
2022-03-29 23:51:26 +0800

    added self intro and update some site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,21 +1,21 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'fluidfeed',
+  author: 'Wendy Chan',
+  headerTitle: 'f l u i d f e e d',
+  description:
+    'A blog created with Next.js, Tailwind and Notion as the CMS, inspired by glassmorphism and water marble arts',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'wenwkich@gmail.com',
+  github: 'https://github.com/wenwkich',
+  // twitter: 'https://twitter.com/Twitter',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
+  // linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
