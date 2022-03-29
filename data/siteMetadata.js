e342290070302676ec9419b0440b08274b003b68
Wendy Chan <wenwkich@gmail.com>
2022-03-30 00:17:10 +0800

    modify site repo information

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteRepo: 'https://github.com/wenwkich/fluidfeed',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   email: 'wenwkich@gmail.com',
