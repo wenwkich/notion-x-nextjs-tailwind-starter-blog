8c53f6924997d1950044ee6fa5a9983a73b9db92
Wendy Chan <wenwkich@gmail.com>
2022-04-01 10:02:50 +0800

    added code component

--- components/NotionComponents.js
+++ components/NotionComponents.js
@@ -1,6 +1,21 @@
 /* eslint-disable react/display-name */
 import { useMemo } from 'react'
 import { NotionRenderer } from 'react-notion-x'
+import dynamic from 'next/dynamic'
+
+const Code = dynamic(() => import('react-notion-x/build/third-party/code').then((m) => m.Code))
+const Collection = dynamic(() =>
+  import('react-notion-x/build/third-party/collection').then((m) => m.Collection)
+)
+const Equation = dynamic(() =>
+  import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
+)
+const Pdf = dynamic(() => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf), {
+  ssr: false,
+})
+const Modal = dynamic(() => import('react-notion-x/build/third-party/modal').then((m) => m.Modal), {
+  ssr: false,
+})
 
 export const NotionLayout = ({ layout, children, ...rest }) => {
   const Layout = require(`../layouts/${layout}`).default
@@ -9,7 +24,19 @@ export const NotionLayout = ({ layout, children, ...rest }) => {
 
 export const NotionLayoutRenderer = ({ layout, recordMap, darkMode, ...rest }) => {
   const NotionComp = useMemo(
-    () => () => <NotionRenderer recordMap={recordMap} darkMode={darkMode} />,
+    () => () =>
+      (
+        <NotionRenderer
+          recordMap={recordMap}
+          darkMode={darkMode}
+          components={{
+            Code,
+            Equation,
+            Modal,
+            Pdf,
+          }}
+        />
+      ),
     [recordMap, darkMode]
   )
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
+import 'prismjs/themes/prism-tomorrow.css'
 
 import '@fontsource/inter/variable-full.css'
 
